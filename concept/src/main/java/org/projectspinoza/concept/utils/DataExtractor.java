e44b11024ee27aff4600425580d78b48693c9ade
MAK <m.aamir.it@gmail.com>
2016-01-20 12:56:50 +0500

    minor updation

--- concept/src/main/java/org/projectspinoza/concept/utils/DataExtractor.java
+++ concept/src/main/java/org/projectspinoza/concept/utils/DataExtractor.java
@@ -20,8 +20,7 @@ public class DataExtractor {
         try {
             url = new URL("http://conceptnet5.media.mit.edu/data/5.3/c/en/"+ keyword);
             String line;
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    url.openStream(), "UTF-8"));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
             if (reader.toString() == "") {
                 System.out.println("No result found");
                 System.exit(0);
@@ -41,82 +40,13 @@ public class DataExtractor {
     }
 
     public static ConceptNet parse(String response) {
-
         ConceptNet conepts = new ConceptNet();
         mapper = new ObjectMapper();
         try {
-            conepts = mapper.readValue(response,
-                    new TypeReference<ConceptNet>() {});
-
+            conepts = mapper.readValue(response, new TypeReference<ConceptNet>() {});
         } catch (Exception e) {
             e.printStackTrace();
         }
         return conepts;
     }
-
-//    @SuppressWarnings("unchecked")
-//    public List<TagConcept> getSurfaceText(ConceptNet concepts, String word) {
-//
-//        List<TagConcept> tagConcepts = new ArrayList<TagConcept>();
-//
-//        for (Object edge : concepts.getEdges()) {
-//            try {
-//                Map<String, Object> attributes = (Map<String, Object>) edge;
-//
-//                String relation = (String) attributes.get("rel");
-//                String start = (String) attributes.get("start");
-//                String end = (String) attributes.get("end");
-//                Double weight = (Double) attributes.get("weight");
-//                String surfaceText = (String) attributes.get("surfaceText");
-//                if (surfaceText != null) {
-//                    surfaceText =  surfaceText.replaceAll("[\\[\\]]", "");
-//                    relation = relation.replace("/r/", "");
-//                    start = start.replace("/c/en/", "");
-//                    end = end.replace("/c/en/", "");
-//
-//                    if (tagConcepts.isEmpty()) {
-//                        if (start.contains(word)) {
-//                            tagConcepts.add(new TagConcept("start", relation, weight, surfaceText));
-//                        } else if (end.contains(word)) {
-//                            tagConcepts.add(new TagConcept("end", relation, weight, surfaceText));
-//                        }
-//                    } else {
-//                        for (int j = 0; j < tagConcepts.size(); j++) {
-//
-//                            if (tagConcepts.get(j).getRelationtType().equals(relation)) {
-//
-//                                Double old_weight_start = tagConcepts.get(j).getWeightStart();
-//                                Double old_weight_end = tagConcepts.get(j).getWeightEnd();
-//
-//                                if (start.contains(word)
-//                                        && weight > old_weight_start) {
-//                                    tagConcepts.get(j).setWeight_start(weight);
-//                                    tagConcepts.get(j).setSurfaceTextStart(surfaceText);
-//
-//                                } else if (end.contains(word)
-//                                        && weight > old_weight_end) {
-//                                    tagConcepts.get(j).setWeightEnd(weight);
-//                                    tagConcepts.get(j).setSurfaceTextEnd(surfaceText);
-//                                }
-//                                break;
-//                            } else if (j != tagConcepts.size() - 1) {
-//                                continue;
-//                            } else {
-//                                if (start.contains(word)) {
-//                                    tagConcepts.add(new TagConcept("start", relation, weight, surfaceText));
-//                                } else if (end.contains(word)) {
-//                                    tagConcepts.add(new TagConcept("end", relation, weight, surfaceText));
-//                                }
-//                                break;
-//                            }
-//                        }
-//                    }
-//                }
-//            } catch (Exception ex) {
-//                ex.printStackTrace();
-//            }
-//        }
-//        return tagConcepts;
-//    }
-
 }
