3a30b5da89e90f343c3cbeaf790e5b963c417372
uMklami <m.aamir.it@gmail.com>
2016-10-16 13:48:53 +0800

    space added

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@
 #version        :1.0    
 #usage          :bash run.sh
 #==============================================================================
+
 java -Xmx128m -jar ./ontology/target/ontology-v1.0-jar-with-dependencies.jar tweets.txt ontology.json
